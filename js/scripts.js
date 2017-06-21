37b850698b080b25e40014f9deeea122a7717e97
Jacek <jacek.paszek@onet.pl>
2017-06-21 22:45:24 +0200

    code shortenning

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,10 @@
-var span = $("span");
+$('span:even').css('color', 'red');
 
-span.each(function(index, element) {
-	if (index % 2 == 0) {
-		$(element).css('color', 'red');
-	};
+$('p').each(function(index, element) {
+    $('<button/>').addClass('btn').attr({ 'data-tmp': index }).text('Click me');
+    $(element).append('<button/>');
 });
 
-var paragraphs = $('p');
-paragraphs.each(function(index, element) {
-
-    var button = '<button class="btn" data-tmp="' + index + '">Click me</button>'
-    $(element).append(button) 
-
-});
-
-$("button").click(function(){
-	alert($(this).attr("data-tmp"));
+$('button').click(function(){
+    alert($(this).attr('data-tmp'));
 });
